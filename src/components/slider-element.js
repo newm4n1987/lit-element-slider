1ab34caed06069488a8ac65adcc1981d85c56729
unknown <newm4n1987@gmail.com>
2019-03-24 17:50:10 +0100

    feat add animation next move out

--- src/components/slider-element.js
+++ src/components/slider-element.js
@@ -8,23 +8,48 @@ class SliderElement extends LitElement {
         display: block;
         text-align: center;
       }
-      .wrapper-image img {
+      .wrapper-image {
+        display: flex;
+        justify-content: center;
+        height: 250px;
+        margin-bottom: 20px;
+      }
+      img {
         border-radius: 50%;
         height: 250px;
         width: 250px;
         object-fit: cover;
+      }
+
+      /* This is the animation code. */
+      .move {
+        animation: move 1.5s ease;
+      }
+
+      @keyframes move {
+        0% {
+            transform: translateX(0);
+        }
+        25% {
+            transform: translateX(-150px);
+            opacity: 0;
+        }
+        50% {
+            transform: translateX(150px);
+            opacity: 0;
+        }
+        70% {
+            transform: translateX(0);
+            opacity: 1;
+        }
       }
     `;
   }
 
   static get properties() {
     return {
-      items: {
-        type: Array
-      },
-      index: {
-        type: Number
-      }
+      items: Array,
+      index: Number
     };
   }
 
@@ -50,19 +75,31 @@ class SliderElement extends LitElement {
     `;
   }
   prev() {
-    if(this.index <= 0) {
+    if (this.index <= 0) {
       this.index = 0;
-    }else {
+    } else {
       this.index--;
     }
   }
 
   next() {
-    if(this.index == (this.items.length-1)) {
-      this.index = this.items.length-1;
-    }else {
-      this.index++;
-    }
+    this.animationMoveOut();
+
+    setTimeout(() => {
+      if (this.index == (this.items.length - 1)) {
+        this.index = this.items.length - 1;
+      } else {
+        this.index++;
+      }
+    },500);
+  }
+
+  animationMoveOut() {
+    const img = this.shadowRoot.querySelector('img');
+    img.classList.add('move');
+    setTimeout(() => {
+      img.removeAttribute('class')
+    }, 1000);
   }
 
   printSlider() {
